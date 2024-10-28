c18dd1be359d41e0c350d572bd2bfb113e31750b
William Rutkowski <budski82@users.noreply.github.com>
2024-10-28 16:09:56 -0400

    almost

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+((_name) =>{
+  let names = [ "Bill", "Anna", "Peter"]
+  let _name = names['']; 
+
+    if(_name === '' || ``) {
+       let reply = _name.names[1]
+
+      const _msg = `Hello World I mean ${reply}`
+    }
+    return _msg
+})
+
+();
+
+
+
+console.log(_msg);
+
+console.log((_name.names[2]));
+
+console.log(reply);
+
+
 console.log(125643);
