d46178f2963551f8b03ea582c040471dba019a52
William Rutkowski <budski82@users.noreply.github.com>
2024-10-28 16:39:13 -0400

    Yes!!

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-((_name) =>{
-  let names = [ "Bill", "Anna", "Peter"]
-  let _name = names['']; 
+function mathApply(a,b,c,d){
+    let operator = [
+        {a:'+'},
+         {b:'-'}, 
+         {c:'*'}, 
+         {d:'/'}
+    ]
 
-    if(_name === '' || ``) {
-       let reply = _name.names[1]
-
-      const _msg = `Hello World I mean ${reply}`
-    }
-    return _msg
-})
-
-();
+    let x = 2; let w = 4;
+    return ` If my code is correct we will see that 2 ${operator[0].a} 4 = 6.`
+}
 
 
+//console.log(_msg);
 
-console.log(_msg);
+//console.log((name.names[2]));
 
-console.log((_name.names[2]));
-
-console.log(reply);
+//console.log(reply);
 
 
 console.log(125643);
